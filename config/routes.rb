da717f34aa8802381c914767fb32453a261b5b50
Dan Roberts <dan@adadevelopersacademy.org>
2017-09-18 12:31:29 -0700

    Create initial routes, none of them do anything yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/books', to: 'books#index'
+  get '/books/new', to: 'books#new'
+  post '/books', to: 'books#create'
+
+  get '/books/:id', to: 'books#show'
+  get '/books/:id/edit', to: 'books#edit'
+  put '/books/:id', to: 'books#update'
+  delete '/books/:id', to: 'books#destroy'
+
+  # resources :books
 end
