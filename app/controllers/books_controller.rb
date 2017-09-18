5ae5214d05217e3ed3d05713d2f9e3b2d766f007
Dan Roberts <dan@adadevelopersacademy.org>
2017-09-18 13:49:50 -0700

    Add some fake data to simulate interacting with the model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
   def index
+    @books = [
+      { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+      { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+      { title: "Kindred", author: "Octavia E. Butler"}
+    ]
   end
 
   def new
