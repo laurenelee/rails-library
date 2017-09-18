8d79fa73bbda999e18bbd10abf3aa1a50b2a1d07
Dan Roberts <dan@adadevelopersacademy.org>
2017-09-18 12:08:31 -0700

    Set up initial rails project

---
+++ config/application.rb
@@ -0,0 +1,22 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module RailsLibrary
+  class Application < Rails::Application
+    config.generators do |g|
+      # Force new test files to be generated in the minitest-spec style
+      g.test_framework :minitest, spec: true
+
+      # Always use .js files, never .coffee
+      g.javascript_engine :js
+    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
