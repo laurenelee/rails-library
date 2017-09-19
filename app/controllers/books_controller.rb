1f9ce3ccc6ec98a38dfecd1be9556b0e58dae82d
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2017-09-19 15:12:15 -0700

    update Book index to query DB with Book.all

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = [
-      { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-      { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-      { title: "Kindred", author: "Octavia E. Butler"}
-    ]
+    @books = Book.all
   end
 
   def new
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
+# app/models/book.rb
 class Book < ApplicationRecord
+  #Already done for us: attr_accessor :title, :author, :description
+  
 end
