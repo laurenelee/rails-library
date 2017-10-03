1a2d2f4892e217f16e42077aafb91aa3f0267ed3
Dan Roberts <dan@adadevelopersacademy.org>
2017-10-03 09:26:02 -0700

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+authors = [
+  {
+    name: "Margot Lee Shetterly",
+    nationality: "unknown"
+  },
+  {
+    name: "Sandi Metz",
+    nationality: "USA"
+  },
+  {
+    name: "Octavia E. Butler",
+    nationality: "USA"
+  }
+]
+
+authors.each do |author|
+  books = author.delete(books)
+
+  a = Author.create!(author)
+  puts "Author: #{a.inspect}"
+end
+
+puts "Created #{Author.count} authors"
