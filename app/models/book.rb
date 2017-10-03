ba783df82c7626b74000200d6afa2a7c13b3efc0
Dan Roberts <dan@adadevelopersacademy.org>
2017-10-03 12:14:03 -0700

    Add validations to books

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,7 @@ class Book < ApplicationRecord
 
   belongs_to :author
 
+  validates :title, presence: true, uniqueness: { message: "The title was not unique! Please choose a different one" }
+  validates :description, length: { maximum: 500, message: "The description is too long" }
+
 end
