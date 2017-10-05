bd036dc868087fcb20d5853cb8d43191b0230b68
Dan Roberts <dan@adadevelopersacademy.org>
2017-10-05 10:15:21 -0700

    Fix bug

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,6 +3,15 @@ class Author < ApplicationRecord
 
   # This is a comment! I'm commenting
   def first_published
-    return books.order(:publication_year).first.publication_year
+    first_book = books.order(:publication_year).first
+    return first_book ? first_book.publication_year : nil
+  end
+
+  # Override the ActiveRecord version to destroy
+  # dependent records as well
+  def destroy
+    puts "Destroying all books for author #{id}"
+    books.destroy_all
+    super
   end
 end
